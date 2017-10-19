3dd9a4c48ee7a49c42c57e78e9f8f5bbcf501608
axionl <axionl@aosc.io>
2017-10-19 11:59:48 +0800

    add "Class admin"

--- src/dbServer.py
+++ src/dbServer.py
@@ -12,7 +12,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-
 class creatTable():
     def __init__(self, config):
         self.config = config
@@ -39,6 +38,7 @@ class creatTable():
             logger.info("Table admininfo existed")
             pass
 
+
 class pushDB():
     def __init__(self, config):
         self.config = config
@@ -51,23 +51,77 @@ class pushDB():
             sys.exit(1)
 
     def push(self, content):
-        check = self.db.query(
-            "SELECT id FROM joininfo WHERE ingress_id=\'{}\'".format(content['ingress_id']))
-        check_id = check.getresult()
-        check_id = []
-        if len(check_id) == 0:
+        _check = self.db.query(
+            "SELECT id FROM joininfo WHERE ingress_id=\'{}\';".format(content['ingress_id']))
+        _check_id = _check.getresult()
+        if len(_check_id) == 0:
             self.db.insert('joininfo', ingress_id=content['ingress_id'], telegram_id=content['telegram_id'],
-                      telegram_username=content['telegram_username'], area=content['area'], other=content['other'])
+                           telegram_username=content['telegram_username'], area=content['area'], other=content['other'])
         else:
-            self.db.query("UPDATE joininfo SET telegram_username=\'{}\' area=\'{}\' other=\'{}\' WHERE ingress_id=\'{}\'".format(
-                content['telegram_username'], content['area'], content['other'], content['telegram_id']))
+            self.db.query("UPDATE joininfo SET telegram_username=\'{}\', area=\'{}\', other=\'{}\' WHERE ingress_id=\'{}\';".format(
+                content['telegram_username'], content['area'], content['other'], content['ingress_id']))
 
-class dbControl(pushDB, creatTable):
+
+class admin():
+    def __init__(self, config):
+        self.config = config
+        try:
+            self.db = DB(
+                dbname=config.database(), user=config.user(),
+                passwd=config.password(), host=config.host())
+        except ConnectionError:
+            logger.info("conntect postgre db failed")
+            sys.exit(1)
+
+    def creatAdmin(self):
+        for i in self.config.admin():
+            _check = self.db.query(
+                "SELECT telegram_id FROM admininfo WHERE telegram_id=\'{}\';".format(i['telegram_id']))
+            _check_id = _check.getresult()
+            if len(_check_id) == 0:
+                self.db.insert(
+                    'admininfo', telegram_id=i['telegram_id'],
+                    telegram_username=i['telegram_username'], area=i['area'])
+                logger.info("add admin member: %s" % i)
+            else:
+                self.db.query(
+                    "UPDATE admininfo SET telegram_username=\'{}\', area=\'{}\' WHERE telegram_id=\'{}\';".format(i['telegram_username'], i['area'], i['telegram_id']))
+                logger.info(
+                    "admin: %s update telegram_username: %s area: %s" %
+                    (i['telegram_id'], i['telegram_username'],i['area']))
+
+    def checkAdmin(self, content):
+        _check = self.db.query(
+            "SELECT telegram_id, area FROM admininfo WHERE telegram_id=\'{}\'".format(content['telegram_id']))
+        _check_id = _check.getresult()
+        if _check_id == []:
+            logger.info("telegram_id: %s is not admin" %
+                        content['telegram_id'])
+            return False
+        else:
+            logger.info("telegram_id: %s is admin" % content['telegram_id'])
+            return True
+
+    def pushAdminId(self, content):
+        _check = self.db.query(
+            "SELECT telegram_id FROM admininfo WHERE area=\'{}\'".format(content['area']))
+        _check_id = _check.getresult()
+        telegram_id = []
+        if len(_check_id) == 0:
+            logger.info("telegram_id: {}, area {} doesn't existed".format(content['telegram_id'], content['area']))
+        else:
+            for i in _check_id:
+                telegram_id.append(i[0])
+            return telegram_id
+
+
+class dbControl(pushDB, creatTable, admin):
     pass
+
 
 if __name__ == '__main__':
     from parseCfg import parseCfg
-    path = 'example.config.yml'
+    path = 'src/example.config.yml'
     config = parseCfg(path)
     db = dbControl(config)
     db.creat()
@@ -75,6 +129,10 @@ if __name__ == '__main__':
         'area': 'T',
         'ingress_id': 'ArielAxionL',
         'other': 'Balthild',
-        'telegram_id': '11111111',
+        'telegram_id': '82814392',
         'telegram_username': 'ADA_Refactor'}
     db.push(content)
+    db.creatAdmin()
+    db.checkAdmin(content)
+    telegram_id = db.pushAdminId(content)
+    print(telegram_id)
