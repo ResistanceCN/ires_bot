ed6692ea7f4c15fec68f9cddc1d4b4f610b5d98b
axionl <axionl@aosc.io>
2017-10-22 22:45:18 +0800

    Bug fixed

--- src/dbServer.py
+++ src/dbServer.py
@@ -45,8 +45,12 @@ class pushDB():
 
     def push(self, content):
         with self.db.cursor() as cur:
-            _check = cur.execute("SELECT id FROM joininfo WHERE ingress_id=%s", (content['ingress_id'],))
+            _check = cur.execute(
+                "SELECT id FROM joininfo WHERE telegram_id=%s",
+                (content['telegram_id'],))
             _check_id = cur.fetchall()
+
+            # TODO: Verify the google account.
             if len(_check_id) == 0:
                 cur.execute(
                     'INSERT INTO joininfo (ingress_id,telegram_id,telegram_username,area,other) VALUES (%s,%s,%s,%s,%s)',
@@ -54,8 +58,8 @@ class pushDB():
                      content['telegram_username'], content['area'],
                      content['other'],))
             else:
-                cur.execute("UPDATE joininfo SET telegram_username=%s, area=%s, other=%s WHERE ingress_id=%s",
-                            (content['telegram_username'], content['area'], content['other'], content['ingress_id'],))
+                cur.execute("UPDATE joininfo SET ingress_id=%s, telegram_username=%s, area=%s, other=%s WHERE telegram_id=%s",
+                            (content['ingress_id'], content['telegram_username'], content['area'], content['other'], content['telegram_id'],))
             self.db.commit()
 
 
@@ -71,9 +75,12 @@ class admin():
             sys.exit(1)
 
     def creatAdmin(self):
+        """Creat admin info from config file."""
         with self.db.cursor() as cur:
             for i in self.config.admin():
-                _check = cur.execute("SELECT telegram_id FROM admininfo WHERE telegram_id=%s", (i['telegram_id'],))
+                _check = cur.execute(
+                    "SELECT telegram_id FROM admininfo WHERE telegram_id=%s",
+                    (i['telegram_id'],))
                 _check_id = cur.fetchall()
                 if len(_check_id) == 0:
                     cur.execute(
@@ -90,33 +97,54 @@ class admin():
             self.db.commit()
 
     def checkAdmin(self, telegram_id):
+        """Check user is admin or not."""
         with self.db.cursor() as cur:
-            _check = cur.execute("SELECT telegram_id, area FROM admininfo WHERE telegram_id=%s", (telegram_id,))
+            _check = cur.execute(
+                "SELECT telegram_id FROM admininfo WHERE telegram_id=%s",
+                (telegram_id,))
             _check_id = cur.fetchall()
             if _check_id == []:
                 logger.info("telegram_id: %s is not admin" %
                             telegram_id)
                 return False
             else:
-                logger.info("telegram_id: %s is admin" % telegram_id)
                 return True
 
     def getAdminId(self, content):
+        """Get admin telegram_id."""
         telegram_id = []
+        columns = ['telegram_id']
+
         with self.db.cursor() as cur:
             for i in list(set(content['area'].replace(' ', '').split(','))):
-                _check = cur.execute("SELECT telegram_id FROM admininfo WHERE area=%s", (i.upper(),))
-                _check_id = cur.fetchall()
-                if len(_check_id) == 0:
-                    logger.info("telegram_id: {}, area {} doesn't existed"
-                                .format(content['telegram_id'], content['area']))
-                else:
-                    for j in _check_id:
-                        telegram_id.append(j[0])
-        return telegram_id
+                _check = cur.execute(
+                    "SELECT telegram_id FROM admininfo WHERE area ILIKE \'{}\'"
+                    .format('%' + i.upper() + '%'))
+                for row in cur.fetchall():
+                    if len(row) == 0:
+                        logger.info(
+                            "telegram_id: {}, area {} doesn't existed"
+                            .format(content['telegram_id'], content['area']))
+                        pass
+                    else:
+                        telegram_id.append(dict(zip(columns, row))['telegram_id'])
+            return telegram_id
 
-    def checkNew(self, telegram_id):
-        # TODO: 
+    def checkNew(self, area):
+        """Check new form"""
+        results = []
+        with self.db.cursor() as cur:
+            columns = ['ingress_id', 'telegram_id', 'telegram_username', 'area', 'other']
+            _check = cur.execute(
+                "SELECT ingress_id, telegram_id, telegram_username, \
+                area, other FROM joininfo WHERE area ILIKE \'{}\' \
+                ORDER BY created_time DESC LIMIT 3;".format(area))
+            for row in cur.fetchall():
+                if len(row) == 0:
+                    pass
+                else:
+                    results.append(dict(zip(columns, row)))
+            return results
 
 class dbControl(pushDB, creatTable, admin):
     pass
@@ -125,7 +153,7 @@ class dbControl(pushDB, creatTable, admin):
 if __name__ == '__main__':
     from parseCfg import parseCfg
 
-    path = '../config.example.yml'
+    path = 'config.example.yml'
     config = parseCfg(path)
     db = dbControl(config)
     db.creat()
@@ -140,3 +168,4 @@ if __name__ == '__main__':
     db.checkAdmin(content['telegram_id'])
     telegram_id = db.getAdminId(content)
     print(telegram_id)
+    print(db.checkNew('%B%'))
