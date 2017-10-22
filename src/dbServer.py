858b8aa3d34defd5a04e6978394d35deffc11d92
imlonghao <shield@fastmail.com>
2017-10-22 16:02:03 +0800

    Upgrade to PyGreSQL DB-API 2.0

--- src/dbServer.py
+++ src/dbServer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pg import DB
+from pg import InternalError
+from pgdb import connect as DB
 import sys
 import logging
 
@@ -17,26 +18,18 @@ class creatTable():
         self.config = config
         try:
             self.db = DB(
-                dbname=config.database(), user=config.user(),
-                passwd=config.password(), host=config.host())
-        except ConnectionError:
+                database=config.database(), user=config.user(),
+                password=config.password(), host=config.host())
+        except InternalError:
             logger.info("conntecting postgresql service failed")
             sys.exit(1)
 
     def creat(self):
-        if 'public.joininfo' not in self.db.get_tables():
-            logger.info("Table joininfo created")
-            self.db.query(self.config.joininfo_table())
-        else:
-            logger.info("Table joininfo existed")
-            pass
-
-        if 'public.admininfo' not in self.db.get_tables():
-            logger.info("Table admininfo created")
-            self.db.query(self.config.admininfo_table())
-        else:
-            logger.info("Table admininfo existed")
-            pass
+        with self.db.cursor() as cur:
+            logger.info('Creating joininfo and adminifo table')
+            cur.execute(self.config.joininfo_table())
+            cur.execute(self.config.admininfo_table())
+            self.db.commit()
 
 
 class pushDB():
@@ -44,21 +37,26 @@ class pushDB():
         self.config = config
         try:
             self.db = DB(
-                dbname=config.database(), user=config.user(),
-                passwd=config.password(), host=config.host())
-        except ConnectionError:
+                database=config.database(), user=config.user(),
+                password=config.password(), host=config.host())
+        except InternalError:
             logger.info("conntect postgre db failed")
             sys.exit(1)
 
     def push(self, content):
-        _check = self.db.query("SELECT id FROM joininfo WHERE ingress_id=%s", (content['ingress_id'],))
-        _check_id = _check.getresult()
-        if len(_check_id) == 0:
-            self.db.insert('joininfo', ingress_id=content['ingress_id'], telegram_id=content['telegram_id'],
-                           telegram_username=content['telegram_username'], area=content['area'], other=content['other'])
-        else:
-            self.db.query("UPDATE joininfo SET telegram_username=%s, area=%s, other=%s WHERE ingress_id=%s",
-                          (content['telegram_username'], content['area'], content['other'], content['ingress_id'],))
+        with self.db.cursor() as cur:
+            _check = cur.execute("SELECT id FROM joininfo WHERE ingress_id=%s", (content['ingress_id'],))
+            _check_id = cur.fetchall()
+            if len(_check_id) == 0:
+                cur.execute(
+                    'INSERT INTO joininfo (ingress_id,telegram_id,telegram_username,area,other) VALUES (%s,%s,%s,%s,%s)',
+                    (content['ingress_id'], content['telegram_id'],
+                     content['telegram_username'], content['area'],
+                     content['other'],))
+            else:
+                cur.execute("UPDATE joininfo SET telegram_username=%s, area=%s, other=%s WHERE ingress_id=%s",
+                            (content['telegram_username'], content['area'], content['other'], content['ingress_id'],))
+            self.db.commit()
 
 
 class admin():
@@ -68,51 +66,53 @@ class admin():
             self.db = DB(
                 dbname=config.database(), user=config.user(),
                 passwd=config.password(), host=config.host())
-        except ConnectionError:
+        except InternalError:
             logger.info("conntect postgre db failed")
             sys.exit(1)
 
     def creatAdmin(self):
-        for i in self.config.admin():
-            _check = self.db.query("SELECT telegram_id FROM admininfo WHERE telegram_id=%s", (i['telegram_id'],))
-            _check_id = _check.getresult()
-            if len(_check_id) == 0:
-                self.db.insert(
-                    'admininfo', telegram_id=i['telegram_id'],
-                    telegram_username=i['telegram_username'], area=i['area'])
-                logger.info("add admin member: %s" % i)
-            else:
-                self.db.query(
-                    "UPDATE admininfo SET telegram_username=%s, area=%s WHERE telegram_id=%s",
-                    (i['telegram_username'], i['area'], i['telegram_id'],))
-                logger.info(
-                    "admin: %s update telegram_username: %s area: %s" %
-                    (i['telegram_id'], i['telegram_username'], i['area']))
+        with self.db.cursor() as cur:
+            for i in self.config.admin():
+                _check = cur.execute("SELECT telegram_id FROM admininfo WHERE telegram_id=%s", (i['telegram_id'],))
+                _check_id = cur.fetchall()
+                if len(_check_id) == 0:
+                    cur.execute(
+                        'INSERT INTO admininfo (telegram_id,telegram_username,area) VALUES (%s,%s,%s)',
+                        (i['telegram_id'], i['telegram_username'], i['area'],))
+                    logger.info("add admin member: %s" % i)
+                else:
+                    cur.execute(
+                        "UPDATE admininfo SET telegram_username=%s, area=%s WHERE telegram_id=%s",
+                        (i['telegram_username'], i['area'], i['telegram_id'],))
+                    logger.info(
+                        "admin: %s update telegram_username: %s area: %s" %
+                        (i['telegram_id'], i['telegram_username'], i['area']))
+            self.db.commit()
 
     def checkAdmin(self, telegram_id):
-        _check = self.db.query(
-            "SELECT telegram_id, area FROM admininfo WHERE telegram_id=%s", (telegram_id,))
-        _check_id = _check.getresult()
-        if _check_id == []:
-            logger.info("telegram_id: %s is not admin" %
-                        telegram_id)
-            return False
-        else:
-            logger.info("telegram_id: %s is admin" % telegram_id)
-            return True
+        with self.db.cursor() as cur:
+            _check = cur.execute("SELECT telegram_id, area FROM admininfo WHERE telegram_id=%s", (telegram_id,))
+            _check_id = cur.fetchall()
+            if _check_id == []:
+                logger.info("telegram_id: %s is not admin" %
+                            telegram_id)
+                return False
+            else:
+                logger.info("telegram_id: %s is admin" % telegram_id)
+                return True
 
     def getAdminId(self, content):
         telegram_id = []
-        for i in list(set(content['area'].replace(' ', '').split(','))):
-            _check = self.db.query(
-                "SELECT telegram_id FROM admininfo WHERE area=%s", (i.upper(),))
-            _check_id = _check.getresult()
-            if len(_check_id) == 0:
-                logger.info("telegram_id: {}, area {} doesn't existed"
-                            .format(content['telegram_id'], content['area']))
-            else:
-                for j in _check_id:
-                    telegram_id.append(j[0])
+        with self.db.cursor() as cur:
+            for i in list(set(content['area'].replace(' ', '').split(','))):
+                _check = cur.execute("SELECT telegram_id FROM admininfo WHERE area=%s", (i.upper(),))
+                _check_id = cur.fetchall()
+                if len(_check_id) == 0:
+                    logger.info("telegram_id: {}, area {} doesn't existed"
+                                .format(content['telegram_id'], content['area']))
+                else:
+                    for j in _check_id:
+                        telegram_id.append(j[0])
         return telegram_id
 
 
